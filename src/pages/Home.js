8a68b86b6d2aa9e9967e0d2cfc26599bea9ea3af
Kranthi Kumar Mamidala <119031030+kumarmamidala@users.noreply.github.com>
2023-12-24 16:06:29 +0530

    Bid domain div added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import Ads from "../components/Ads";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ContainerFeaturedItems from "../components/Containers/ContainerFeaturedItems";
 import Trending from "../components/Trendinglist/TrendingList";
 import Process from "../components/Process/Process";
@@ -13,11 +13,22 @@ import FlowCards from "../components/FlowCards/FlowCards";
 import { ProductContext } from "../Context/products_context";
 import Shapes from "../components/Shapes/Shapes";
 import Footerr from "../components/Footerr/Footerr";
+import { Link } from "react-router-dom";
+
+import $ from "jquery";
 
 function Home() {
   const productss = useContext(ProductContext);
+
+  useEffect(() => {
+    $(window).on("scroll", function () {
+      $(window).scrollTop()
+        ? $(".trending-div").addClass("scrolled-div")
+        : $(".trending-div").removeClass("scrolled-div");
+    });
+  }, []);
   return (
-    <div className="flex flex-col max-w-10xl mx-auto scroll-smooth">
+    <div className="flex flex-col max-w-10xl mx-auto scroll-smooth home-main-div">
       <TopBar />
       <Hero />
       <div className="ads-flowcards-div">
@@ -36,6 +47,18 @@ function Home() {
       <Discover />
       <Questions data={DataHomeQuestions} />
       <Footerr />
+      <div className="trending-div">
+        <div className="bid-now-div">
+          <Link className="bid-now-div-btn">Bid Now</Link>
+        </div>
+        <div className="trending-domains12">
+          <h1 className="heading">Trending Domains</h1>
+          <p className="paragrpah">Amrapali.gold</p>
+          {/* <p className="paragrpah">bishallmark.gold</p>
+          <p className="paragrpah">tanishq.gold</p>
+          <p className="paragrpah">bsk.gold</p> */}
+        </div>
+      </div>
     </div>
   );
 }
