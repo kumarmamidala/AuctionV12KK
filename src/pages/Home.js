749434e4607db9b7e854868755753820bc43e49d
Kranthi Kumar Mamidala <119031030+kumarmamidala@users.noreply.github.com>
2023-12-26 11:22:06 +0530

    Added Swiper to the trending domains div

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,6 +15,13 @@ import Shapes from "../components/Shapes/Shapes";
 import Footerr from "../components/Footerr/Footerr";
 import { Link } from "react-router-dom";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+
 import $ from "jquery";
 
 function Home() {
@@ -27,6 +34,17 @@ function Home() {
         : $(".trending-div").removeClass("scrolled-div");
     });
   }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+  };
   return (
     <div className="flex flex-col max-w-10xl mx-auto scroll-smooth home-main-div">
       <TopBar />
@@ -53,7 +71,22 @@ function Home() {
         </div>
         <div className="trending-domains12">
           <h1 className="heading">Trending Domains</h1>
-          <p className="paragrpah">Amrapali.gold</p>
+          {/* <Trending Domains Component here /> */}
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1} // Set to 1 to show only one slide at a time
+            autoplay={{ delay: 500, disableOnInteraction: true }}
+          >
+            {productss &&
+              productss.slice(0, 4).map((item, idx) => (
+                <SwiperSlide key={item?.ID}>
+                  {/* Adjust the content inside the slider as needed */}
+                  <h1>{item.Name}</h1>
+                </SwiperSlide>
+              ))}
+          </Swiper>
+
+          {/* Add more slides as needed */}
           {/* <p className="paragrpah">bishallmark.gold</p>
           <p className="paragrpah">tanishq.gold</p>
           <p className="paragrpah">bsk.gold</p> */}
