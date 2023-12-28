c984c362a54b27144eea2b6485245f21ac15ea83
Kranthi Kumar Mamidala <119031030+kumarmamidala@users.noreply.github.com>
2023-12-28 15:54:55 +0530

    Trending Domains div styled

--- src/components/ExploreCard/ExploreCard.js
+++ src/components/ExploreCard/ExploreCard.js
@@ -7,41 +7,43 @@ import "./ExploreCard.css";
 const ExploreCard = () => {
   return (
     <div className="flex justify-between items-center w-full md:px-[60px] gap-5 lg:mt-0 mt-10 pb-10 explore-section">
-      <div className="lg:w-1/2 w-full lg:mb-0 mb-5 lg:!text-left explore-content">
-        <h3 className="text-4xl font-bold !text-[45px] text-[#fbcb04] my-4 title !text-left">
-          <span className="text-primary">Domain Name</span> Value
-        </h3>
-        <hr className="!w-full" />
-        <h2 className="font-medium leading-normal paragraph !normal-case mt-[20px]">
-          We know what web addresses are really worth and every domain is
-          different. Our scientifically sound, expert appraisals – for both
-          individual domains and domain portfolios.
-        </h2>
-        {/* <p className="my-3 paragraph">
+      <div className="flex explore-inner-div">
+        <div className="lg:w-1/2 w-full lg:mb-0 mb-5 lg:!text-left explore-content">
+          <h3 className="text-4xl font-bold !text-[45px] text-[#fbcb04] my-4 title !text-left">
+            <span className="text-primary">Domain Name</span> Value
+          </h3>
+          <hr className="!w-full" />
+          <h2 className="font-medium leading-normal paragraph !normal-case mt-[20px]">
+            We know what web addresses are really worth and every domain is
+            different. Our scientifically sound, expert appraisals – for both
+            individual domains and domain portfolios.
+          </h2>
+          {/* <p className="my-3 paragraph">
           The question both buyers and sellers ask us is: How much can a domain
           cost?
         </p> */}
 
-        <div className="flex flex-wrap md:ml-0 items-center gap-[20px] mt-[20px] explore-ul">
-          <p className="flex items-center gap-1 paragraph ">
-            <BiMap className="text-primary text-2xl" />
-            <span>Our domain evaluation is incredibly fast</span>
-          </p>
+          <div className="flex flex-wrap md:ml-0 items-center gap-[20px] mt-[20px] explore-ul">
+            <p className="flex items-center gap-1 paragraph ">
+              <BiMap className="text-primary text-2xl" />
+              <span>Our domain evaluation is incredibly fast</span>
+            </p>
 
-          <p className="flex items-center gap-1 paragraph ">
-            <BiBriefcase className="text-primary text-2xl" />
-            <span>Our services are affordable</span>
-          </p>
+            <p className="flex items-center gap-1 paragraph ">
+              <BiBriefcase className="text-primary text-2xl" />
+              <span>Our services are affordable</span>
+            </p>
 
-          <p className="flex items-center gap-1 paragraph ">
-            <BiFolder className="text-primary text-2xl" />
-            <span>We provide scientifically backed results</span>
-          </p>
+            <p className="flex items-center gap-1 paragraph ">
+              <BiFolder className="text-primary text-2xl" />
+              <span>We provide scientifically backed results</span>
+            </p>
+          </div>
+          <button className="red-btn">Get In Touch</button>
         </div>
-        <button className="red-btn">Get In Touch</button>
-      </div>
-      <div className="lg:w-1/2 w-full explore-image">
-        <img src={image} alt="" className="appraisal-hero-img" />
+        <div className="lg:w-1/2 w-full explore-image">
+          <img src={image} alt="" className="appraisal-hero-img" />
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import Ads from "../components/Ads";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import ContainerFeaturedItems from "../components/Containers/ContainerFeaturedItems";
 import Trending from "../components/Trendinglist/TrendingList";
 import Process from "../components/Process/Process";
@@ -15,6 +15,8 @@ import Shapes from "../components/Shapes/Shapes";
 import Footerr from "../components/Footerr/Footerr";
 import { Link } from "react-router-dom";
 
+import loader from "./../components/Cards/loader-timer-main.gif";
+
 import AwesomeSlider from "react-awesome-slider";
 import withAutoplay from "react-awesome-slider/dist/autoplay";
 import "react-awesome-slider/dist/styles.css";
@@ -24,6 +26,13 @@ import $ from "jquery";
 function Home() {
   const productss = useContext(ProductContext);
   const AutoplaySlider = withAutoplay(AwesomeSlider);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (productss && productss.length > 0) {
+      setLoading(false);
+    }
+  }, [productss]);
 
   useEffect(() => {
     $(window).on("scroll", function () {
@@ -52,21 +61,39 @@ function Home() {
       <Footerr />
       <div className="trending-div">
         <div className="bid-now-div">
-          <Link className="bid-now-div-btn">Bid Now</Link>
+          <Link to="/featured" className="bid-now-div-btn">
+            Bid Now
+          </Link>
         </div>
         <div className="trending-domains12">
-          <h1 className="heading">Trending Domains</h1>
+          <h1 className="heading mb-[10px]">Trending Domains</h1>
           {/* <Trending Domains Component here /> */}
-          <AutoplaySlider
-            play={true}
-            cancelOnInteraction={false} // should stop playing on user interaction
-            interval={2000}
-            organicArrows={false}
-            bullets={false}
-          >
-            {productss &&
-              productss.slice(0, 4).map((item, idx) => <div className="car-sel">{item.Name}</div>)}
-          </AutoplaySlider>
+          {loading ? (
+            <div className="">
+              <img src={loader} alt="" className="w-[60px] m-auto" />
+              {/* <p className="paragraph text-black !m-auto text-center">
+                Loading  .gold domains...
+              </p> */}
+            </div>
+          ) : (
+            <AutoplaySlider
+              play={true}
+              cancelOnInteraction={false} // should stop playing on user interaction
+              interval={2000}
+              organicArrows={false}
+              bullets={false}
+            >
+              {productss &&
+                productss.slice(0, 4).map((item, idx) => (
+                  <div className="car-sel">
+                    <Link to={`/Marketplace/${item.ID}`}>
+                      {item.Name}
+                      {/* <p>Rs.{item.Price}</p> */}
+                    </Link>
+                  </div>
+                ))}
+            </AutoplaySlider>
+          )}
         </div>
       </div>
     </div>
