ee4579a2734d3450157dce1557a5bbe3b59042f1
Kranthi Kumar Mamidala <119031030+kumarmamidala@users.noreply.github.com>
2023-12-29 15:41:27 +0530

    Added Loader comment in domain auctions page

--- src/components/DomainSearch/ProductList.js
+++ src/components/DomainSearch/ProductList.js
@@ -4,48 +4,19 @@ import GridView from "./GridView";
 
 const ProductList = () => {
   const { filtered_products: products, grid_view } = useFilterContext();
-  console.log(products);
 
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate an asynchronous fetch of products
-    const fetchProducts = async () => {
-      try {
-        // Fetch your products here (replace the setTimeout with your actual API call)
-        await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulating a 2-second delay
-        // Once products are fetched, set loading to false
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-        // Handle error if needed
-        setLoading(false);
-      }
-    };
-
-    // Call the fetchProducts function
-    fetchProducts();
-  }, []); // The empty dependency array ensures that this effect runs only once on component mount
+    // Check if products are available to determine if data has been loaded
+    if (products.length > 0) {
+      setLoading(false);
+    }
+  }, [products]); // Use products as a dependency
 
   if (loading) {
-    return (
-      <h5
-        style={{ fontSize: "2.5rem", fontWeight: "bold" }}
-        className="paragraph text-white mt-[20px] ml-[20px]"
-      >
-        Loading the products...
-      </h5>
-    );
+    return <h2 className="loading-comment">Loading the products...</h2>; // You can customize the loading indicator as needed
   }
-
-  if (products.length < 1) {
-    return (
-      <h5 style={{ fontSize: "2.5rem", fontWeight: "bold" }}>
-        No Products found..
-      </h5>
-    );
-  }
-
   return <GridView products={products}>product list</GridView>;
 };
 
